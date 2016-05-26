19ccbf1b73c1ab8fb6291074e324e5b4c26fc4ae
Luke Pearce <luke.pearce@teamwpc.co.uk>
2016-05-26 15:30:43 +0100

    Add bigint support for SQL Server

--- lib/bigint_pk.rb
+++ lib/bigint_pk.rb
@@ -41,6 +41,10 @@ module BigintPk
     if ca.const_defined? :AbstractMysqlAdapter
       ca::AbstractMysqlAdapter::NATIVE_DATABASE_TYPES[:primary_key].gsub! /int\(11\)/, 'bigint(20)'
     end
+
+    if ca.const_defined? :SQLServerAdapter
+      ActiveRecord::Base.connection.native_database_types[:primary_key].sub! /^int/, 'bigint'
+    end
   end
 
   def self.install_foreign_key_patches!
@@ -58,7 +62,7 @@ module BigintPk
           #                                                             t.string  :subject_type, null: false
           #   t.references :subject, polymorphic: { limit: 120 }    ==> t.integer :subject_id, limit: 8
           #                                                             t.string  :subject_type, limit: 120
-          options[:polymorphic] = options.except(:polymorphic, :limit) if options[:polymorphic] == true 
+          options[:polymorphic] = options.except(:polymorphic, :limit) if options[:polymorphic] == true
           references_without_default_bigint_fk( *args, options )
         end
         alias_method_chain :references, :default_bigint_fk
@@ -84,6 +88,34 @@ module BigintPk
         MODIFY COLUMN #{c.quote_column_name key_name}
         bigint(20) #{is_primary_key ? 'auto_increment' : 'DEFAULT NULL'}
       }.gsub(/\s+/, ' ').strip
+    when 'SQLServer'
+      if is_primary_key
+        # For primary keys we need to drop & recreate the PK constraint
+        c.execute %Q{
+          DECLARE @constraint_name AS NVARCHAR(255)
+
+          SET @constraint_name = (
+            SELECT kc.Name
+            FROM sys.tables t
+            INNER JOIN sys.key_constraints kc ON t.object_id = kc.parent_object_id
+            INNER JOIN sys.columns c ON kc.parent_object_id = c.object_id
+            WHERE t.Name = '#{c.quote_string table_name}' and c.Name = '#{c.quote_string key_name}'
+          )
+
+          EXEC('ALTER TABLE #{c.quote_table_name table_name} DROP ' + @constraint_name)
+
+          ALTER TABLE #{c.quote_table_name table_name}
+          ALTER COLUMN #{c.quote_column_name key_name} bigint
+
+          ALTER TABLE #{c.quote_table_name table_name}
+          ADD PRIMARY KEY (#{c.quote_column_name key_name})
+        }.gsub(/\s+/, ' ').strip
+      else
+        c.execute %Q{
+          ALTER TABLE #{c.quote_table_name table_name}
+          ALTER COLUMN #{c.quote_column_name key_name} bigint
+        }.gsub(/\s+/, ' ').strip
+      end
     when 'SQLite'
       # noop; sqlite always has 64bit pkeys
     else
